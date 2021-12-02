3595955c0c71ad4527e57cac780d83f5e62a3c38
Bryce Slade <bryceslade@gmail.com>
2021-12-02 10:51:09 -0700

    testing GET

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+
+const server = express()
+
+server.get('/api/users', (req, res) =>{
+    res.json([
+        {id: 1, username: 'nelson'},
+        {id: 2, username: 'bryce'},
+        {id: 3, username: 'slade'}
+    ])
+})
+
+server.post('/api/register', (req, res) => {
+
+})
+
+const PORT = process.env.PORT || 8080
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
