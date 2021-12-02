b5e7f3ebdd266742ffb50771c5b5ec662f314739
Bryce Slade <bryceslade@gmail.com>
2021-12-02 11:23:02 -0700

    reset

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 require('dotenv').config()
 const express = require('express')
-const User = require('./users/model')
 
 const server = express()
 
-server.get("/api/users", async ( req, res ) => {
-    try{
-        const users = await User.find()
-        res.status(200).json(users)
-
-    } catch(error) {
-        res.status(500).json( { message: "The user information could not be retrieved" } )
-    }
+server.get('/api/users', (req, res) =>{
+    res.json([
+        {id: 1, username: 'nelson'},
+        {id: 2, username: 'bryce'},
+        {id: 3, username: 'slade'}
+    ])
 })
 
-server.post("/api/register", async (req, res) => {
-    try {
-        const { username, password } = req.body
+server.post("/api/register", (req, res) => {
+})
 
-        if(!username || !password) {
-            res.status(400).json( { message: "Please provide username and bio for the user" } )
-
-        } else {
-            const user = await User.insert( {username, password } )
-            res.status(201).json(user)
-        }
-
-    } catch(error) {
-        res.status(500).json( { message: "The user with the specified ID does not exist" } )
-    }
+server.post("/api/login", (req, res) => {
+    
 })
 
 const PORT = process.env.PORT || 8080
